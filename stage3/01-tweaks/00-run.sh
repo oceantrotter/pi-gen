36a537b5674648039b1345cefd47f44f1ef4b340
Romain Bazile <gromain.baz@gmail.com>
2020-09-17 16:48:17 +0200

    create test and libraries directories in user /home/

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -e
 
 rm -f "${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/wait.conf"
+
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/test"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/libraries"
